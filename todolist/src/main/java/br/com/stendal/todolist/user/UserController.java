e78708fee916788e86b32f26cea2797598eaff25
unknown <stendalcassio@hotmail.com>
2023-10-10 12:20:44 -0300

    feat: autenticação do userModel e adição do bd

--- todolist/src/main/java/br/com/stendal/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/stendal/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.stendal.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.name);
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exist");
+    } else {
+      var userCreated = this.userRepository.save(userModel);
+      return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
   }
 }
