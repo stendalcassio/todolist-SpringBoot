8b1d166c0362083bdb999d8673e7f678ba8ba2d3
Stendal <stendalcassio@hotmail.com>
2023-10-13 11:38:11 -0300

    validações e adição do DELETE nas tasks

--- todolist/src/main/java/br/com/stendal/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/stendal/todolist/task/TaskController.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,13 +52,45 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+  public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
 
     var task = this.taskRepository.findById(id).orElse(null);
 
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Tarefa não encontrada");
+    }
+
+    var idUser = request.getAttribute("idUser");
+
+    if (!task.getIdUser().equals(idUser)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Usuário não tem permissão para alterar essa tarefa");
+    }
     Utils.copyNonNullProperties(taskModel, task);
 
-    return this.taskRepository.save(task);
+    var taskUpdated = this.taskRepository.save(task);
+    return ResponseEntity.ok().body(taskUpdated);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity delete(@PathVariable UUID id, HttpServletRequest request) {
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body("Tarefa não encontrada");
+    }
+
+    var idUser = request.getAttribute("idUser");
+
+    if (!task.getIdUser().equals(idUser)) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+          .body("Usuário não tem permissão para deletar essa tarefa");
+    }
+
+    this.taskRepository.deleteById(id);
+    return ResponseEntity.status(HttpStatus.OK).body("Tarefa deletada com sucesso");
   }
 
 }
--- todolist/src/main/java/br/com/stendal/todolist/task/TaskModel.java
+++ todolist/src/main/java/br/com/stendal/todolist/task/TaskModel.java
@@ -14,7 +14,7 @@ import lombok.Data;
 @Data
 @Entity(name = "tb_tasks")
 public class TaskModel {
-  
+
   @Id
   @GeneratedValue(generator = "UUID")
   private UUID id;
@@ -30,5 +30,12 @@ public class TaskModel {
 
   @CreationTimestamp
   private LocalDateTime createdAt;
+
+  public void setTitle(String title) throws Exception {
+    if (title.length() > 50) {
+      throw new Exception("O campo title deve conter no máximo 50 caracteres");
+    }
+    this.title = title;
+  }
 
 }
